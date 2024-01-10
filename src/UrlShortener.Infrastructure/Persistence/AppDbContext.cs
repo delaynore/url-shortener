055c9ba4cac2a8709ba0d04b1584802c22a3f597
DanyaLozhkin <delaynore@gmail.com>
2024-01-10 22:04:21 +0400

    feat: add DbContext

---
+++ src/UrlShortener.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using UrlShortener.Domain.Models;
+
+namespace UrlShortener.Infrastructure.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Link> Links => Set<Link>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
