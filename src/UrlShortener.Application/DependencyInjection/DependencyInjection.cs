7266709423bbc94bf44a2db433f886790ce8a6a8
DanyaLozhkin <delaynore@gmail.com>
2024-01-10 22:02:48 +0400

    feat: add application di

---
+++ src/UrlShortener.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Application.Interfaces.Services;
+using UrlShortener.Application.Services;
+
+namespace UrlShortener.Application.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IUrlService, UrlService>();
+        return services;
+    }
+}
