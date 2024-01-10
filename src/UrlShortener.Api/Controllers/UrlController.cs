9094c624c2ebdabd435f1e5e24932f728fbb15ee
DanyaLozhkin <delaynore@gmail.com>
2024-01-10 23:00:40 +0400

    feat: add controller

---
+++ src/UrlShortener.Api/Controllers/UrlController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Application.Dto;
+using UrlShortener.Application.Interfaces.Services;
+
+namespace UrlShortener.Api.Controllers;
+
+[Route("")]
+[ApiController]
+public class UrlController(IUrlService urlService) : ControllerBase
+{
+    private readonly IUrlService _urlService = urlService;
+    
+    [HttpGet("")]
+    public async Task<IActionResult> ShortenUrl([FromQuery] UrlDto urlDto)
+    {
+        var shortenUrl = await _urlService.ShortenUrl(urlDto, default);
+        var host = HttpContext.Request.Host.ToString();
+        return Ok(new UrlDto(host + "/" + shortenUrl.Url));
+    }
+    
+    [HttpGet("{shortUrl}")]
+    public async Task<IActionResult> ShortenUrl(string shortUrl)
+    {
+        var urlDto = new UrlDto(shortUrl);
+        var originalUrl = await _urlService.ExpandUrl(urlDto, default);
+        return Ok(originalUrl);
+    }
+}
