ab1d2be8b4c1921c019691ec85e03722dd9541cb
DanyaLozhkin <delaynore@gmail.com>
2024-01-10 22:02:28 +0400

    feat: realize services

---
+++ src/UrlShortener.Application/Services/UrlService.cs
@@ -0,0 +1,27 @@
+﻿using UrlShortener.Application.Dto;
+using UrlShortener.Application.Interfaces.Repositories;
+using UrlShortener.Application.Interfaces.Services;
+using UrlShortener.Domain.Models;
+
+namespace UrlShortener.Application.Services;
+
+public class UrlService(IUrlRepository repository, IUrlShortener urlShortener) : IUrlService
+{
+    public async Task<UrlDto> ShortenUrl(UrlDto originalUrl, CancellationToken cancellationToken = default)
+    {
+        var original = Url.Create(originalUrl.Url);
+        var shorted = await urlShortener.Short(original);
+        
+        var link = Link.Create(0, original, shorted); //some questions about id = 0
+        return new UrlDto((await repository.Create(link, cancellationToken)).Value);
+    }
+
+    public async Task<UrlDto> ExpandUrl(UrlDto shortUrl, CancellationToken cancellationToken = default)
+    {
+        var url = Url.Create(shortUrl.Url);
+        var expandedUrl = await repository.GetByShortUrl(url, cancellationToken);
+        
+        if (expandedUrl is null) throw new Exception(shortUrl + " is not found");
+        return new UrlDto(expandedUrl.Value);
+    }
+}
---
+++ src/UrlShortener.Infrastructure/Services/UrlShortener.cs
@@ -0,0 +1,23 @@
+﻿using UrlShortener.Application.Interfaces.Services;
+using UrlShortener.Domain.Models;
+
+namespace UrlShortener.Infrastructure.Services;
+
+public class UrlShortener : IUrlShortener
+{
+    private const int ShortUrlLength = 6;
+
+    private const string AllowedCharacters = "1234567890" +
+                                             "qwertyuiopasdfghjklzxcvbnm" +
+                                             "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private Url GetShortenUrl(Url _)
+    {
+        var random = Random.Shared;
+        return Url.Create(new string(random.GetItems<char>(AllowedCharacters, ShortUrlLength)));
+    }
+
+    public Task<Url> Short(Url originalUrl)
+    {
+        return new Task<Url>(() => GetShortenUrl(originalUrl));
+    }
+}
