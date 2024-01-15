e8a77e7cd92c550c071ca90f46c548bb9b3db37a
DanyaLozhkin <delaynore@gmail.com>
2024-01-15 19:14:56 +0400

    docs: add docs for controller

--- src/UrlShortener.Api/Controllers/UrlController.cs
+++ src/UrlShortener.Api/Controllers/UrlController.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using UrlShortener.Application.Dto;
 using UrlShortener.Application.Interfaces.Services;
 
 namespace UrlShortener.Api.Controllers;
 
 [Route("")]
+[Consumes("application/json")]
+[Produces("application/json")]
 public class UrlController(IUrlService urlService) : ControllerBase
 {
     private readonly IUrlService _urlService = urlService;
-    
+
+    /// <summary>
+    /// Shorten the original url.
+    /// </summary>
+    /// <param name="urlDto">The original url.</param>
+    /// <returns>The shorten url.</returns>
+    [ProducesResponseType(typeof(UrlDto),StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpGet("")]
     public async Task<IActionResult> ShortenUrl([FromQuery] UrlDto urlDto)
     {
@@ -16,7 +26,14 @@ public class UrlController(IUrlService urlService) : ControllerBase
         var host = HttpContext.Request.Host.ToString();
         return Ok(new UrlDto(host + "/" + shortenUrl.Url));
     }
-    
+
+    /// <summary>
+    /// Get the original url by short url.
+    /// </summary>
+    /// <param name="shortUrl">The short url.</param>
+    /// <returns>The original url.</returns>
+    [ProducesResponseType(typeof(UrlDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpGet("{shortUrl}")]
     public async Task<IActionResult> ShortenUrl(string shortUrl)
     {
